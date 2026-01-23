ce1307f7581256bf6a1b9be0b2247e30ecf54421
Michele Maffei <michele.muffin@gmail.com>
2026-01-23 16:18:44 +0100

    fix: remove conflicting script hash from headers

--- build.js
+++ build.js
@@ -11,14 +11,8 @@ const minified = await minify(html, {
   minifyCSS: true,
 });
 
-const scriptMatch = minified.match(/<script>(.*?)<\/script>/g);
-
-const scriptContent = scriptMatch ? scriptMatch[scriptMatch.length - 1].replace(/<\/?script>/g, '') : '';
-
-const scriptHash = crypto.createHash('sha256').update(scriptContent).digest('base64');
-
 const headers = `/*
-  Content-Security-Policy: default-src 'none'; script-src https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com 'sha256-${scriptHash}' https://whatever-dev-ws.github.io 'unsafe-eval' 'unsafe-inline' blob:; connect-src https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com https://fonts.googleapis.com data:; img-src data:; font-src https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com https://fonts.gstatic.com; style-src 'unsafe-inline'; worker-src blob:; base-uri 'none'; form-action 'none'; frame-ancestors https://whatever-dev-ws.github.io
+  Content-Security-Policy: default-src 'none'; script-src https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com https://whatever-dev-ws.github.io 'unsafe-eval' 'unsafe-inline' blob:; connect-src https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com https://fonts.googleapis.com data:; img-src data:; font-src https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com https://fonts.gstatic.com; style-src 'unsafe-inline'; worker-src blob:; base-uri 'none'; form-action 'none'; frame-ancestors https://whatever-dev-ws.github.io
   X-Content-Type-Options: nosniff`;
 
 fs.mkdirSync('dist', { recursive: true });
